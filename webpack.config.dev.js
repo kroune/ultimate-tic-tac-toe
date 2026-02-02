ea68e64cc770ac47dc02557c5c20956f53462fb1
kroune <123553746+kroune@users.noreply.github.com>
2026-02-02 21:20:36 +0300

    .

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -10,4 +11,21 @@ module.exports = merge(common, {
     open: true,
     static: ['./'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: { // Максимальное сжатие HTML
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      }
+    }),
+  ]
 });
